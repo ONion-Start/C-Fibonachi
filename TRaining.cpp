2025304b3424c3cc7804671433f37e98aa18b4fd
Timofey <135414755+ONion-Start@users.noreply.github.com>
2023-12-02 16:22:42 +0300

    Add files via upload

---
+++ TRaining.cpp
@@ -0,0 +1,22 @@
+﻿#include<iostream>
+int main() {
+	short numBefore, numAfter,numClose;
+	std::cout << "Input number up to 27656" << std::endl;
+	std::cin >> numClose;
+	numBefore = 1;
+	numAfter = 1;
+	std::cout << 1  <<" " << 1 << " ";
+	while (numBefore < numClose || numAfter < numClose) {
+		numBefore += numAfter;
+		if (numBefore>numClose) {
+			break;
+		}
+		std::cout <<numBefore << " ";
+		numAfter += numBefore ;
+		if (numAfter > numClose) {
+			break;
+		}
+		std::cout << numAfter << " ";
+	}
+	return 0;
+}
